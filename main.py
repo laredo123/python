2aeb7985ca315e9928b0de2ea51229d26ffea570
laredo123 <gerardocs@hotmail.com>
2016-12-08 11:24:15 -0600

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
 def mayor(n1,n2,n3):
-    if :
-        
+    if n1,n2,n3 > 3:
+        return
+        print ""
     else:
-        
+        print "error"
 n1 : raw_input("enter number --->")
 n2 : raw_input("enter number --->")
 n3 : raw_input("enter number --->")
